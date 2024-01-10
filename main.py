cadad22da142d109d400fbbb8f6a5c85f8e9f90c
shibby360 <shivank.chhaya@gmail.com>
2024-01-09 20:43:08 -0800

    I DONT NEED IDENTIFICATION GENERATION LOL

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, redirect, request
 from bson.objectid import ObjectId
 import pymongo
 import json
-import idgen
 import os
 if os.path.isfile('mongouri.txt'):
   connectionstring = open('mongouri.txt').read().strip()
